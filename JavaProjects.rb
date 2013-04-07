b80ad774748eceb92bff97ec490446568ca3261d
peterk <peterk@livingwork.com>
2013-04-07 12:49:02 -0400

    cleaning java files

--- JavaProjects.rb
+++ JavaProjects.rb
@@ -39,31 +39,33 @@ public
     def doCompileJava(t)
 
         config = t.config;
+        outClasspath = getRelativePath(config.outputClasspath);
 
         cmdline = "\"#{config.jdk_}/bin/javac.exe\"";
-        cmdline << " -g -d \"#{config.outputClasspath}\""
+        cmdline << " -g -d \"#{outClasspath}\""
+
+        paths = config.classPaths
+        unless(paths.empty?)
+            cmdline << " -classpath \"#{outClasspath}";
+            paths.each do |path|
+                cmdline << ";#{getRelativePath(path)}"
+            end
+            cmdline << "\"";
+        end
 
         paths = config.sourceRoots
         unless(paths.empty?)
             prepend = " -sourcepath \"";
             paths.each do |path|
-                cmdline << "#{prepend}#{path}"
+                cmdline << "#{prepend}#{getRelativePath(path)}"
                 prepend = ';'
             end
             cmdline << "\"";
         end
 
-        paths = config.classPaths
-        unless(paths.empty?)
-            cmdline << " -classpath \"#{outputClasspath}";
-            paths.each do |path|
-                cmdline << ";#{path}"
-            end
-            cmdline << "\"";
-        end
 
         t.sources.each do |src|
-            cmdline << " \"#{src}\"";
+            cmdline << " \"#{getRelativePath(src)}\"";
         end
 
         puts("#{cmdline}") # if verbose?
@@ -75,6 +77,11 @@ public
         def needed?
             !sources.empty?
         end
+    end
+
+    # Are there any tasks with an earlier time than the given time stamp?
+    def any_task_earlier?(tasks,time)
+        tasks.any? { |n| puts("check #{n}"); n.timestamp < time }
     end
 
 
@@ -95,8 +102,16 @@ public
             t.config.sources << t.source
         end
 
+        if(any_task_earlier?(tasks,File.mtime(File.expand_path(__FILE__))))
+            puts("project is altered");
+        end
+
         tsk.enhance(tasks);
         tsk.config = self;
+
+		task :clean do
+			addCleanFiles(tasks);
+		end
 
         tsk;
     end
