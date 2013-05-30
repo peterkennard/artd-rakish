b645142a61122cab3091166d3faf3ffdb1cd28da
peterk <peterk@livingwork.com>
2013-05-30 15:42:16 -0400

    initializing VC compiler confiuration

--- WindowsCppTools.rb
+++ WindowsCppTools.rb
@@ -49,14 +49,51 @@ LoadableModule.onLoaded(Module.new do
 
 			splitcfgs = args[:split];
 					
-			platform  = splitcfgs[0];
-			platformType = args[:platform];
-			platformBits = args[:platformBits];
+			@platform  = splitcfgs[0];
+			@platformType = args[:platformType];
+			@platformBits = args[:platformBits];
 		
-			compiler  = args[:compiler];
-			linkType  = args[:linkType];
-			debugType = args[:debugType];
+			@compiler  = args[:compiler];
+			@linkType  = args[:linkType];
+			@debugType = args[:debugType];
 							
+		end
+
+		def ensureConfigOptions(cfg)
+	
+			cfg.cppDefine(
+				'ARTD_WINDOWS=',						
+				'WIN32', 
+				'_LIB', 
+				'WINVER=0x0501', 
+				'_WIN32_WINNT=0x0501', 
+				'_WIN32_WINDOWS=0x0410', 
+				'_WIN32_IE=0x0600',
+				'_WINDOWS',
+				'_USRDLL',
+				'_FILE_OFFSET_BITS=64', 
+				'_CRT_SECURE_NO_DEPRECATE', 
+				'_CRT_NONSTDC_NO_DEPRECATE',
+				'_MBCS',
+				'_UNICODE', 
+				'UNICODE',
+				'NOMINMAX',
+				'_SCL_SECURE_NO_WARNINGS', 
+				'STRSAFE_NO_DEPRECATE',
+                "ARTD_PLATFORMTYPE=#{@platformType}",
+                "ARTD_PLATFORMBITS=#{@platformBits}",
+				"ARTD_COMPILERTYPE=#{@compiler}",
+				"ARTD_LINKAGETYPE=#{@linkType}",
+				"ARTD_DEBUGTYPE=#{@debugType}"
+			);
+			
+			tpp = cfg.thirdPartyPath;
+
+			cfg.addIncludePaths(
+				"#{tpp}/include/Win32",
+				"#{tpp}/include"
+			);
+
 		end
 
 		def initCompileTask(cfg)
@@ -113,17 +150,18 @@ LoadableModule.onLoaded(Module.new do
 
 		platformBits = '32';
 		if(cfgs[0] =~ /\d+/)
-			platform = $`;
+			platformType = $`;
 			platformBits = $&;
 		end
-		if(platform === 'Win')
-			platform = 'Windows';
+		if(platformType === 'Win')
+			platformType = 'Windows';
 		end
 
 		log.debug { "config validated #{cfgs.join('-')}" };
 		return( Win32Tools.new( 
 			:split=>cfgs,
 			:platformBits=>platformBits,
+			:platformType=>platformType,
 			:compiler=>compiler,
 			:linkType=>linkType,
 			:debugType=>debugType
