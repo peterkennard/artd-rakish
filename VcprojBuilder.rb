c111de39456527a7f10984e836e2c5c47cd9439e
peterk <peterk@livingwork.com>
2013-06-03 07:40:38 -0400

    added filters to vcprojects

--- VcprojBuilder.rb
+++ VcprojBuilder.rb
@@ -164,6 +164,29 @@ EOTEXT
 		end
 	end
 
+
+	@@rakefileFiltersTxt_=<<EOTEXT
+<?xml version="1.0" encoding="utf-8"?>
+<Project ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <ItemGroup>
+    <Filter Include="Source Files">
+      <UniqueIdentifier>{4FC737F1-C7A5-4376-A066-2A32D752A2FF}</UniqueIdentifier>
+      <Extensions>cpp;c;cc;cxx;def;odl;idl;hpj;bat;asm;asmx</Extensions>
+    </Filter>
+    <Filter Include="Header Files">
+      <UniqueIdentifier>{93995380-89BD-4b04-88EB-625FBE52EBFB}</UniqueIdentifier>
+      <Extensions>h;hpp;hxx;hm;inl;inc;xsd</Extensions>
+    </Filter>
+    <Filter Include="Resource Files">
+      <UniqueIdentifier>{67DA6AB6-F800-4c08-8B7A-83BB121AAD01}</UniqueIdentifier>
+      <Extensions>rc;ico;cur;bmp;dlg;rc2;rct;bin;rgs;gif;jpg;jpeg;jpe;resx;tiff;tif;png;wav;mfcribbon-ms</Extensions>
+    </Filter>
+  </ItemGroup>
+  <ItemGroup>
+  </ItemGroup>
+</Project>
+EOTEXT
+
 	attr_reader :cppProject
 	attr_reader :rakeCommandLine
 
@@ -177,13 +200,19 @@ EOTEXT
 		rakeFile = vcprojRelative(proj.projectFile);
 
 		@rakeCommandLine = "#{rakeCommand} -f #{rakeFile} \"RakishBuildRoot=$(SolutionDir)build\"";
+		
 		rubyLinePP(@@rakefileConfigTxt_,file,binding());
+	end
+
+	def writeRakefileVCProjFilters(file)
+		rubyLinePP(@@rakefileFiltersTxt_,file,binding());
 	end
 
 	def writeVCProjFiles(proj)
 		@cppProject = proj;
 
 		defpath = File.join(proj.vcprojDir, proj.moduleName + '-rake.vcxproj'); 
+		filpath = "#{defpath}.filters"
 		tempPath = File.join(proj.vcprojDir, proj.moduleName + '.temp');
 		
 		puts(" creating vcproj in #{defpath}")
@@ -193,6 +222,12 @@ EOTEXT
 			end	
 			if(textFilesDiffer(tempPath,defpath))
 				mv(tempPath, defpath);
+			end
+			File.open(tempPath,'w') do |f|
+				writeRakefileVCProjFilters(f);
+			end	
+			if(textFilesDiffer(tempPath,filpath))
+				mv(tempPath, filpath);
 			end
 		rescue => exception
 		  puts exception
@@ -208,7 +243,9 @@ EOTEXT
 	end
 	
 	def VcprojBuilder.onVcprojCleanTask(proj)
-		puts(" cleaning vcproj ");
+		defpath = File.join(proj.vcprojDir, proj.moduleName + '-rake.vcxproj'); 
+		filpath = "#{defpath}.filters";
+		proj.addCleanFiles(defpath,filpath);		
 	end 
 
 end
