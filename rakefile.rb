6ec7340971f871794accafe4411e392483b5df84
peterk <peterk@livingwork.com>
2016-03-02 18:57:05 -0500

    work on gem packaging

---
+++ rakefile.rb
@@ -0,0 +1,20 @@
+require 'rake'
+
+include Rake::DSL
+
+task :default do 
+end
+
+task :installGem do |t|
+	myDir = File.dirname(__FILE__);
+	
+	spec = Gem::Specification::load("#{myDir}/rakish.gemspec")
+	puts spec.version
+	
+	cd myDir do
+		system("gem build rakish.gemspec");
+		system("gem install rakish-#{spec.version}.gem");
+	end
+end
+
+
