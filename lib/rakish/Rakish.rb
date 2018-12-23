b33dbd06c1eeb6115555844d2fbe14d5e4ff4f2d
peterk <peterk@livingwork.com>
2018-12-23 08:50:34 -0500

    added GitModule

--- lib/rakish/Rakish.rb
+++ lib/rakish/Rakish.rb
@@ -843,44 +843,6 @@ module Rakish
 		include ::Rake::DSL
 		include Rakish::Logger
 
-		# Very simple module for Git used to initialize my projects
-		module Git # :nodoc:
-
-			class << self
-				def clone(src,dest,opts={})
-					if(!File.directory?(dest))
-
-						origin = opts[:remote] || "origin";
-
-						puts("Git.clone -o \"#{origin}\" -n \"#{src}\" \"#{dest}\"");
-
-						system("git clone -o \"#{origin}\" -n \"#{src}\" \"#{dest}\"");
-						cd dest do
-							system("git config -f ./.git/config --replace-all core.autocrlf true");
-							system("git reset -q --hard");
-						end
-					end
-				end
-
-				def addRemote(dir, name, uri)
-					cd dir do
-						system("git remote add \"#{name}\" \"#{uri}\"");
-					end
-                end
-
-				def checkout(branch, opts={})
-                    cmd = "git checkout \"#{branch}\"";
-					if(opts[:dir])
-					    cd opts[:dir] do
-                            system(cmd);
-                        end
-                     else
-                        system(cmd);
-                     end
-                end
-			end
-		end
-
         # convenience method like Rake::task
         def task(*args,&block)
             Rake::Task.define_task(*args, &block)
