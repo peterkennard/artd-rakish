67652c371ff3762fd4195bfb2f049ed0ef7c33a2
peterk <peterk@livingwork.com>
2023-03-31 22:58:17 -0400

    suppress password requestor from ssh/git when cloning.

--- lib/rakish/GitModule.rb
+++ lib/rakish/GitModule.rb
@@ -11,13 +11,26 @@ module Rakish
 			class << self
 				def clone(src,dest,opts={})
 					if(!File.directory?(dest))
+
 						origin = opts[:remote] || "origin";
+                        silent = opts[:silent];
 
 						puts("Git.clone -o \"#{origin}\" -n \"#{src}\" \"#{dest}\"");
-						# system("git clone -o \"#{origin}\" -n \"#{src}\" \"#{dest}\"");
-						ret = `git clone -o \"#{origin}\" -n \"#{src}\" \"#{dest}\" 2>&1`
+
+                        ret = '';
+
+                        oCommand = ENV['GIT_SSH_COMMAND']
+                        begin
+                            ENV['GIT_SSH_COMMAND'] = "ssh -o PasswordAuthentication=no"
+ 						    ret = `git clone -o \"#{origin}\" -n \"#{src}\" \"#{dest}\" 2>&1`
+                        rescue
+                        ensure
+                            ENV['GIT_SSH_COMMAND'] = oCommand;
+                        end
 					    if(ret =~ /fatal\:/)
-					        log.debug("#{ret}");
+					        unless(silent)
+					            log.debug("#{ret}");
+					        end
 					        return false;
 					    end
 					    if(!File.directory?(dest))
@@ -33,7 +46,11 @@ module Rakish
 				end
 
 				def cloneIfAvailable(src,dest,opts={})
-			        clone(src,dest,opts);
+                    # You can disable password authentication with -o PasswordAuthentication=no. Full command would be:
+                    # ssh -nT -o PasswordAuthentication=no <host>  # n no stdout output, T no tty input.
+                    # GIT_SSH_COMMAND=ssh -o PasswordAuthentication=no' controls what git does
+                    opts[:silent] = true;
+                    clone(src,dest,opts);
 				end
 
 				def addRemote(dir, name, uri)
